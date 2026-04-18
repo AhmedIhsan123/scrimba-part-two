ca1f6d262d1c7bb190363fcc299ecd307d20f760
Ahmed Ihsan <Ihsan.Ahmed@student.greenriver.edu>
2026-04-17 23:37:06 -0700

    Made progress on scrimba

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,31 @@
 import "../styles/Main.css";
+import React from "react";
 
 export default function () {
+	const [ingredients, setIngredients] = React.useState([]);
+	const ingredientsListItems = ingredients.map((ingredient) => (
+		<li key={ingredient}>{ingredient}</li>
+	));
+
+	function addIngredient(formData) {
+		const newIngredient = formData.get("ingredient");
+		setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+	}
+
 	return (
 		<>
 			<main className="main-container">
-				<form className="ingredients-form">
+				<form action={addIngredient} className="ingredients-form">
 					<input
 						className="ingredients-input"
 						type="text"
 						placeholder="e.g. oregano"
 						aria-label="Add ingredient"
+						name="ingredient"
 					/>
 					<button className="add-ingredients btn">+ Add ingredients</button>
 				</form>
+				<ul className="ingredients-list">{ingredientsListItems}</ul>
 			</main>
 		</>
 	);
